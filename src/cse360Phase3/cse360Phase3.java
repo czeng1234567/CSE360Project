35293da5ecd0faa8886b790cf66d49f4ac1a0218
czeng1234567 <33376346+czeng1234567@users.noreply.github.com>
2024-04-10 03:31:04 -0700

    Added gui Elements
    Added gui elements

--- src/cse360Phase3/cse360Phase3.java
+++ src/cse360Phase3/cse360Phase3.java
@@ -1,5 +1,8 @@
 package cse360Phase3;
 
+//public class cse360Phase3 
+//{
+	//package application;
 
 
 	import javafx.application.Application;
@@ -42,7 +45,11 @@ package cse360Phase3;
 	        //Creates Layout
 	        Label homeTitle = new Label("Welcome to Sunshine Pediatric Center");
 	        homeTitle.setStyle("-fx-font-size: 24px; -fx-alignment: center;");
-	   
+	        //StackPane stackPane = new StackPane(homeTitle);
+	        //stackPane.setAlignment(Pos.CENTER);
+	        //StackPane stackPane = new StackPane(homeTitle);
+	        //stackPane.setAlignment(Pos.CENTER);
+	        //stackPane.setPrefSize(800, 300);
 	        Label empty1 = new Label("");
 	        Label empty2 = new Label("");
 	        Label empty3 = new Label("");
@@ -61,18 +68,22 @@ package cse360Phase3;
 	        btnNS.setStyle(buttonStyle);
 	        btnRP.setStyle(buttonStyle);
 	        btnDR.setStyle(buttonStyle);
-
-		//adding buttons to hbox
+	        
 	        hbox.getChildren().add(btnNP);
 	        hbox.getChildren().add(btnNS);
 	        hbox.getChildren().add(btnRP);
 	        hbox.getChildren().add(btnDR); 
 	        
+	        //---------------------------------------------------------------------
+	        /*
+	         * This part is designated for the New Patient Creation Screen
+	         */
+	        
 	        Random rand = new Random();
 	        
 	        //Labels
-	        //Label npTitle = new Label("New Patient Login");
-	        //npTitle.setStyle("-fx-font-size: 18px; -fx-alignment: center;");
+	        Label npTitle = new Label("New Patient Login");
+	        npTitle.setStyle("-fx-font-size: 18px; -fx-alignment: center;");
 	        Label firstName = new Label("First Name");
 	        Label lastName = new Label("Last Name");
 	        Label email = new Label("email");
@@ -88,12 +99,17 @@ package cse360Phase3;
 	        TextField ad = new TextField();
 	        TextField bd = new TextField();
 	        
+	        //fn.setPrefWidth(150);
+	        
 	        //Formatted into Vbox
 	        VBox textVbox = new VBox(15); 
 	        VBox taVbox = new VBox(7); 
 	        
 	        Button btnSave = new Button("Save");
 	        Button back = new Button("Back");
+	        
+	        //VBox totalTNP = new VBox();
+	        
 	        
 
 	        //textVbox.getChildren().add(npTitle);
@@ -112,6 +128,7 @@ package cse360Phase3;
 	        taVbox.getChildren().add(bd);
 	        taVbox.getChildren().add(btnSave);
 	        taVbox.getChildren().add(back);
+	        
 	        
 	        //Formatted into borderpane (originally was a stackpane)
 	        BorderPane piStack = new BorderPane();
@@ -175,6 +192,7 @@ package cse360Phase3;
 	            }
 	        });
 	        
+	        //totalTNP.getChildren().addAll();
 	        
 	       
 	        
@@ -183,6 +201,7 @@ package cse360Phase3;
 	        /*
 	         * This part is designated for the Patient View Screen
 	         */
+	        Label titleT = new Label("Welcome new patient!");
 	        Label empty = new Label("");
 	        Label editProfile = new Label("Edit Profile: ");
 	        Label firstN = new Label("First Name: ");
@@ -199,7 +218,6 @@ package cse360Phase3;
 	        TextField phoneNField = new TextField();
 	        TextField emaiField = new TextField();
 	        TextField addrField = new TextField();
-	        
 	        
 	        //Formatted into vboxes
 	        VBox textVbox3 = new VBox(7); 
@@ -223,14 +241,28 @@ package cse360Phase3;
 	        /*
 	         * This part is designated for the Patient Login Screen
 	         */
+	        Label namePID = new Label("Welcome returning patient!");
+	        namePID.setStyle("-fx-font-size: 24px; -fx-alignment: center;");
+	        
 	        Label warning = new Label("");
 	        Label pid3 = new Label("Patient ID: ");
 	        TextField pidField2 = new TextField();
-	        Button btnLogin = new Button("Hello world");
+	        pidField2.setMaxWidth(250);
+	        Button btnLogin = new Button("Login");
 	        Button backRP = new Button("Back");
 	        
+	        VBox verticalUsage = new VBox();
+	        verticalUsage.setAlignment(Pos.CENTER);
+	        verticalUsage.getChildren().addAll(namePID, pid3, pidField2, warning);
+	        
 	        HBox hboxLogin = new HBox();
-	        hboxLogin.getChildren().addAll(pid3, pidField2, btnLogin, warning, backRP);
+	        hboxLogin.setAlignment(Pos.CENTER);
+	        hboxLogin.getChildren().addAll(backRP,btnLogin);
+	        
+	        VBox htotal = new VBox();
+	        htotal.setAlignment(Pos.CENTER);
+	        htotal.getChildren().addAll(verticalUsage, hboxLogin); 
+	        
 	        
 	        
 	        //Getting and reading from file comes here. When clicking the login button
@@ -299,13 +331,18 @@ package cse360Phase3;
 	        
 	        VBox num3 = new VBox();
 	        num3.getChildren().addAll(patientVisits, rectangle3); 
+	        
+	        ComboBox<String> upcomingVisits = new ComboBox<>();
+	        StackPane stackPane2 = new StackPane();
+	        stackPane2.getChildren().addAll(num3, upcomingVisits);
+	        
 
 	        StackPane stackPane = new StackPane();
 	        stackPane.getChildren().addAll(num1, patientDropdown);
 
 	        HBox hboxPanes = new HBox();
 	        hboxPanes.setSpacing(10);
-	        hboxPanes.getChildren().addAll(stackPane, num2, num3);
+	        hboxPanes.getChildren().addAll(stackPane, num2, stackPane2);
 	        hboxPanes.setAlignment(Pos.CENTER);
 
 	        
@@ -328,6 +365,98 @@ package cse360Phase3;
 	        
 	        //---------------------------------------------------------------------
 	        
+	        
+	        //implementing doctor
+	        
+	        Label doctorTitle = new Label("Welcome doctor!");
+	        doctorTitle.setStyle("-fx-font-size: 24px;");
+	        doctorTitle.setAlignment(Pos.CENTER); // Center the text within the label
+	        
+	        Label appointmentD = new Label("Appointments");
+	        Button scheduleApp = new Button("Schedule");
+	        Button manageApp = new Button("Manage");
+	        
+	        
+	        
+
+	        Rectangle rectangle4 = new Rectangle(200, 100); // Width, Height
+	        rectangle4.setFill(null); // No fill color
+	        rectangle4.setStroke(Color.BLACK); // Outline color
+	        rectangle4.setStrokeWidth(2);
+	        
+	        StackPane stackPane5 = new StackPane();
+	        stackPane5.getChildren().addAll(rectangle4, appointmentD, scheduleApp, manageApp);
+
+	        // Align the labels and buttons within the StackPane
+	        stackPane5.setAlignment(appointmentD, Pos.CENTER);
+	        stackPane5.setAlignment(scheduleApp, Pos.BOTTOM_LEFT);
+	        stackPane5.setAlignment(manageApp, Pos.BOTTOM_RIGHT); 
+
+	        Rectangle rectangle5 = new Rectangle(200, 100); // Width, Height
+	        rectangle5.setFill(null); // No fill color
+	        rectangle5.setStroke(Color.BLACK); // Outline color
+	        rectangle5.setStrokeWidth(2);
+	        
+	        Label medicalHistoryL = new Label("Patient Medical History");
+	        StackPane stackPane6 = new StackPane();
+	        stackPane6.getChildren().addAll(rectangle5, medicalHistoryL); 
+	        
+	        stackPane6.setAlignment(medicalHistoryL, Pos.TOP_CENTER);
+
+	        Rectangle rectangle6 = new Rectangle(200, 100); // Width, Height
+	        rectangle6.setFill(null); // No fill color
+	        rectangle6.setStroke(Color.BLACK); // Outline color
+	        rectangle6.setStrokeWidth(2);
+	        
+	        Label patientFindingsL = new Label("Patient Findings");
+	        StackPane stackPane7 = new StackPane();
+	        stackPane7.getChildren().addAll(rectangle6, patientFindingsL); 
+	        
+	        stackPane7.setAlignment(patientFindingsL, Pos.TOP_CENTER);
+
+	        Rectangle rectangle7 = new Rectangle(200, 100); // Width, Height
+	        rectangle7.setFill(null); // No fill color
+	        rectangle7.setStroke(Color.BLACK); // Outline color
+	        rectangle7.setStrokeWidth(2);
+	        
+	        Label prescribeMedicineL = new Label("PrescribeMedicine");
+	        StackPane stackPane8 = new StackPane();
+	        stackPane8.getChildren().addAll(rectangle7, prescribeMedicineL); 
+	        
+	        stackPane8.setAlignment(prescribeMedicineL, Pos.TOP_CENTER);
+
+	        HBox doctorRectangles = new HBox();
+	        doctorRectangles.setSpacing(10);
+	        doctorRectangles.getChildren().addAll(stackPane5, stackPane6, stackPane7, stackPane8); 
+	        
+	        Button backD = new Button("Back");
+
+	        VBox doctorSetup = new VBox();
+	        doctorSetup.setAlignment(Pos.CENTER);
+	        doctorSetup.setSpacing(20);
+	        doctorSetup.getChildren().addAll(doctorRectangles, backD); 
+
+	        BorderPane root = new BorderPane();
+	        root.setTop(doctorTitle);
+	        root.setAlignment(doctorTitle, Pos.CENTER); // Center the title horizontally
+	        root.setCenter(doctorSetup);
+
+	        Scene doctorScene = new Scene(root, 800, 300);
+
+	        btnDR.setOnAction(new EventHandler<>() {
+	            public void handle(ActionEvent event) {
+	                primaryStage.setScene(doctorScene);
+	            }
+	        });
+
+
+	        
+	        
+	        
+	        
+	        
+	        
+	        
 	        //Formatting the app
 	        //BorderPane homeScreen = new BorderPane(hbox, homeTitle, empty1, empty2, empty3);
 	        VBox homeLayout = new VBox(10); // 10 pixels spacing between nodes
@@ -337,7 +466,7 @@ package cse360Phase3;
 	        Scene homeScene = new Scene(homeLayout, 800, 300);
 	        
 	        Scene piScene = new Scene(piStack, 800, 300);
-	        Scene pvScene = new Scene(hboxLogin, 800, 300);
+	        Scene pvScene = new Scene(htotal, 800, 300);
 	        
 	        btnNP.setOnAction(new EventHandler<>() {
 	            public void handle(ActionEvent event) {
@@ -370,6 +499,14 @@ package cse360Phase3;
 	        });
 	        
 	        backRP.setOnAction(new EventHandler<>() 
+	        {
+	            public void handle(ActionEvent event) 
+	            {
+	                primaryStage.setScene(homeScene);
+	            }
+	        });
+	        
+	        backD.setOnAction(new EventHandler<>() 
 	        {
 	            public void handle(ActionEvent event) 
 	            {
